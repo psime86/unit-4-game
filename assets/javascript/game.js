e76a652f64f466fb4e5ab9b5d93fd5b5a84036cd
Patrick Sime <psime86@gmail.com>
2020-01-03 22:09:08 -0600

    JS variables and initial HTML

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// Variables
+
+// Crystals x 4 (random number from 1-12)
+var crystal1 = Math.floor(Math.random()*12+1);
+var crystal2 = Math.floor(Math.random()*12+1);
+var crystal3 = Math.floor(Math.random()*12+1);
+var crystal4 = Math.floor(Math.random()*12+1);
+console.log(crystal1,crystal2,crystal3,crystal4)
+
+// W/L and Count
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+
+// Target number (random number between 19-120)
+var target = Math.floor(Math.random()*102+19);
+console.log(target)
+
+
+
+
+
