daeabcddfa6bf27f4d293e50270a4043e5f17a07
Patrick Sime <psime86@gmail.com>
2020-01-04 01:06:19 -0600

    stuck on click event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,45 @@
-// Variables
+// Target number (random number between 19-120)
+$(document).ready(function() {
+    var targetNumber = Math.floor(Math.random()*102+19);
+    console.log(targetNumber)
+
+// Apend target number to game
+    $("#target-number").html(targetNumber);
 
 // Crystals x 4 (random number from 1-12)
-var crystal1 = Math.floor(Math.random()*12+1);
-var crystal2 = Math.floor(Math.random()*12+1);
-var crystal3 = Math.floor(Math.random()*12+1);
-var crystal4 = Math.floor(Math.random()*12+1);
-console.log(crystal1,crystal2,crystal3,crystal4)
+    var userNumber1 = Math.floor(Math.random()*12+1);
+    var userNumber2 = Math.floor(Math.random()*12+1);
+    var userNumber3 = Math.floor(Math.random()*12+1);
+    var userNumber4 = Math.floor(Math.random()*12+1);
+    console.log(userNumber1,userNumber2,userNumber3,userNumber4)
 
 // W/L and Count
-var wins = 0;
-var losses = 0;
-var totalScore = 0;
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
+    console.log(wins,losses,totalScore)
 
-// Target number (random number between 19-120)
-var target = Math.floor(Math.random()*102+19);
-console.log(target)
+// Apend wins and losses to game
+    $("#wins").html(wins);
+    $("#losses").html(losses)
 
+// Updating win total
+    function victory(){
+        alert("Winner winner chicken dinner!");
+        wins++,
+        $("#wins").html(wins)
+    }
 
+// Updating loss total
+    function defeat(){
+        alert("You lost...");
+        losses++;
+        $("#losses").html(losses)
+    }
+})
 
-
+// On click for crystals
+    $(".crystal-image").on("click", function(){
+        alert("this is a click")
+    })
 
