3b7057b7a4f2234c797cf702e6929e008e38955e
Patrick Sime <psime86@gmail.com>
2020-01-04 17:40:58 -0600

    On Click Fixedgit add .!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // Target number (random number between 19-120)
 $(document).ready(function() {
     var targetNumber = Math.floor(Math.random()*102+19);
-    console.log(targetNumber)
+    console.log(targetNumber);
 
 // Apend target number to game
     $("#target-number").html(targetNumber);
@@ -11,35 +11,49 @@ $(document).ready(function() {
     var userNumber2 = Math.floor(Math.random()*12+1);
     var userNumber3 = Math.floor(Math.random()*12+1);
     var userNumber4 = Math.floor(Math.random()*12+1);
-    console.log(userNumber1,userNumber2,userNumber3,userNumber4)
+    console.log(userNumber1,userNumber2,userNumber3,userNumber4);
 
 // W/L and Count
     var wins = 0;
     var losses = 0;
     var totalScore = 0;
-    console.log(wins,losses,totalScore)
+    console.log(wins,losses,totalScore);
+
+    // On click for crystals
+    $("#crystal1").on("click", function() {
+        var newTotal = totalScore += userNumber1;
+        $("#user-score").html(newTotal)
+    })
+
+    $("#crystal2").on("click", function() {
+        var newTotal = totalScore += userNumber2;
+        $("#user-score").html(newTotal)
+    })
+
+    $("#crystal3").on("click", function() {
+        var newTotal = totalScore += userNumber3;
+        $("#user-score").html(newTotal)
+    })
+
+    $("#crystal4").on("click", function() {
+        var newTotal = totalScore += userNumber4;
+        $("#user-score").html(newTotal)
+    })
 
 // Apend wins and losses to game
     $("#wins").html(wins);
-    $("#losses").html(losses)
-
+    $("#losses").html(losses);
+})
 // Updating win total
-    function victory(){
+    function victory() {
         alert("Winner winner chicken dinner!");
-        wins++,
-        $("#wins").html(wins)
+        wins++;
+        $("#wins").html(wins);
     }
 
 // Updating loss total
-    function defeat(){
+    function defeat() {
         alert("You lost...");
         losses++;
-        $("#losses").html(losses)
+        $("#losses").html(losses);
     }
-})
-
-// On click for crystals
-    $(".crystal-image").on("click", function(){
-        alert("this is a click")
-    })
-
