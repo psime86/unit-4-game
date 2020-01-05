685acbcb837583d8ae13c7548eec472ba40873ef
Patrick Sime <psime86@gmail.com>
2020-01-04 21:51:05 -0600

    CSS done reset function not appending

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,41 +19,90 @@ $(document).ready(function() {
     var totalScore = 0;
     console.log(wins,losses,totalScore);
 
-    // On click for crystals
+// Apend wins and losses to game
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+
+// Reset function
+function reset() {
+    targetNumber = Math.floor(Math.random()*102+19);
+    $("#target-number").html(targetNumber);
+    console.log(targetNumber);
+    userNumber1 = Math.floor(Math.random()*12+1);
+    userNumber2 = Math.floor(Math.random()*12+1);
+    userNumber3 = Math.floor(Math.random()*12+1);
+    userNumber4 = Math.floor(Math.random()*12+1);
+    console.log(userNumber1,userNumber2,userNumber3,userNumber4);
+    totalScore = 0;
+    $("#target-number").html(totalScore);
+    console.log(totalScore);
+}
+
+// Win total function
+    function victory() {
+        alert("Winner winner chicken dinner!");
+        wins++;
+        $("#wins").html(wins);
+        reset();
+        console.log(reset);
+    }
+
+// Loss total function
+    function defeat() {
+        alert("You lost...");
+        losses++;
+        $("#losses").html(losses);
+        reset();
+        console.log(reset);
+    }
+
+// On click for crystals
     $("#crystal1").on("click", function() {
         var newTotal = totalScore += userNumber1;
         $("#user-score").html(newTotal)
+// Win condition
+        if (totalScore === targetNumber) {
+            victory();
+        }
+        else if (totalScore > targetNumber) {
+            defeat();
+        }
     })
 
     $("#crystal2").on("click", function() {
         var newTotal = totalScore += userNumber2;
         $("#user-score").html(newTotal)
+
+        if (totalScore === targetNumber) {
+            victory();
+        }
+        else if (totalScore > targetNumber) {
+            defeat();
+        }
     })
 
     $("#crystal3").on("click", function() {
         var newTotal = totalScore += userNumber3;
         $("#user-score").html(newTotal)
+
+        if (totalScore === targetNumber) {
+            victory();
+        }
+        else if (totalScore > targetNumber) {
+            defeat();
+        }
     })
 
     $("#crystal4").on("click", function() {
         var newTotal = totalScore += userNumber4;
         $("#user-score").html(newTotal)
+
+        if (totalScore === targetNumber) {
+            victory();
+        }
+        else if (totalScore > targetNumber) {
+            defeat();
+        }
     })
 
-// Apend wins and losses to game
-    $("#wins").html(wins);
-    $("#losses").html(losses);
 })
-// Updating win total
-    function victory() {
-        alert("Winner winner chicken dinner!");
-        wins++;
-        $("#wins").html(wins);
-    }
-
-// Updating loss total
-    function defeat() {
-        alert("You lost...");
-        losses++;
-        $("#losses").html(losses);
-    }
